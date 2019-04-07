89d4fa409439e6651d6ce24fcc3dd89b1eb64625
Maria <mbosca9@gmail.com>
2019-04-07 06:59:15 -0700

    Third commit

--- PART2/AT/Class1.cs
+++ PART2/AT/Class1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,47 @@ namespace AT
 {
     public class Class1
     {
+
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver(@"F:\Automation\Project\Automation\PART2\AT\Driver");
+            driver.Url = @"file:F:\Automation\Pages\homepage.html";
+
+            // Assert.AreEqual("Home page", driver.Title);
+            StringAssert.AreEqualIgnoringCase("Home page", driver.Title);
+
+            IWebElement emailField = driver.FindElement(By.Id("email"));
+            IWebElement pwdField = driver.FindElement(By.Id("password"));
+
+            emailField.SendKeys("admin@domain.org");
+            pwdField.SendKeys("111111");
+
+            IWebElement loginButton = driver.FindElement(By.Id("Login"));
+
+            loginButton.Click();
+
+            driver.Quit();
+
+
+         
+
+            
+
+
+
+            
+
+            
+
+
+
+
+
+        }
+        
+
+        
+
     }
 }
